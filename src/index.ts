83e04839ad572f4c29fa0b9f8b509ad55830b65d
Gonzalo Barros <gramirezb@gmail.com>
2025-09-16 10:37:41 -0300

    Enable health check with port 8081

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import app from './app';
-// import { setupHealthCheck } from './health';
+import { setupHealthCheck } from './health';
 
 const PORT = parseInt(process.env.PORT || '5001', 10);
 
 // Configurar health check para Railway
-// setupHealthCheck();
+setupHealthCheck();
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
